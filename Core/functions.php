01aebcf5e3d2ef9e6347f435c719d8e035e0d3cf
yonas44 <tesfu.yonas1350@gmail.com>
2023-11-17 20:11:30 +0300

    Refactor routes and make Router a class

--- Core/functions.php
+++ Core/functions.php
@@ -11,6 +11,15 @@ function dd($value)
   die();
 }
 
+function abort($code = 404)
+{
+  http_response_code($code);
+
+  require(base_path("views/{$code}.php"));
+
+  die();
+}
+
 function urlIS($value)
 {
   return $_SERVER['REQUEST_URI'] === $value;
--- views/notes/show.view.php
+++ views/notes/show.view.php
@@ -11,6 +11,7 @@
     </p>
 
     <form method="POST">
+      <input type="hidden" name="_method" value="DELETE">
       <input type="hidden" name="id" value="<?= $note['id'] ?>">
       <button class="text-sm text-red-500 mt-5">Delete</button>
     </form>
