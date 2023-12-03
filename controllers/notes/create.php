236761a327e5b327f13cbd40481b5b6e4cf73bb5
yonas44 <tesfu.yonas1350@gmail.com>
2023-12-03 19:04:08 +0300

    Set up store controller and its route to persist a note

--- controllers/notes/create.php
+++ controllers/notes/create.php
@@ -1,31 +1,6 @@
 <?php
 
-use Core\Database;
-use Core\Validator;
-
-$config = require(base_path('config.php'));
-
-$db = new Database($config['database']);
-
-$errors = [];
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-
-  if (!Validator::string($_POST['body'], 1, 255)) {
-    $errors['body'] = 'A body of not more than 255 characters is required.';
-  }
-
-  if (empty($errors)) {
-    $note = $db->query('INSERT INTO notes(body, user_id) VALUES (:body, :user_id)', [
-      'body' => $_POST['body'],
-      'user_id' => 5
-    ]);
-
-    header('location: /notes');
-  }
-}
-
 view('/notes/create.view.php', [
   'heading' => 'Create a note',
-  'errors' => $errors
+  'errors' => []
 ]);
--- routes.php
+++ routes.php
@@ -4,7 +4,9 @@ $router->get('/', 'controllers/index.php');
 $router->get('/about', 'controllers/about.php');
 
 $router->get('/notes', 'controllers/notes/index.php');
+
 $router->get('/notes/create', 'controllers/notes/create.php');
-$router->post('/notes/create', 'controllers/notes/create.php');
+$router->post('/notes', 'controllers/notes/store.php');
+
 $router->get('/note', 'controllers/notes/show.php');
 $router->delete('/note', 'controllers/notes/destroy.php');
