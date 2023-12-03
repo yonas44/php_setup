7f6fc0b7d183dccb9c9f4871684f32aca190bd9e
yonas44 <tesfu.yonas1350@gmail.com>
2023-12-03 18:48:57 +0300

    Set up routes to route delete and create request methods to their respective controllers

--- routes.php
+++ routes.php
@@ -5,4 +5,6 @@ $router->get('/about', 'controllers/about.php');
 
 $router->get('/notes', 'controllers/notes/index.php');
 $router->get('/notes/create', 'controllers/notes/create.php');
+$router->post('/notes/create', 'controllers/notes/create.php');
 $router->get('/note', 'controllers/notes/show.php');
+$router->delete('/note', 'controllers/notes/destroy.php');
